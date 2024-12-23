6ecf4a9963007f8b2f146f65ff49ef5fca31246e
dbsrjsdn0130 <ygwoo0130@gmail.com>
2024-12-23 15:32:19 +0900

    모터 회전 수정

--- navigation.py
+++ navigation.py
@@ -1,25 +1,23 @@
-from motor_control import set_motor_speed
 import time
+from motor_control import set_motor_direction
 
 def rotate(angle):
-    """로봇 회전"""
-    turn_time = abs(angle) / 90
-    if angle > 0:
-        set_motor_speed("left", 50)
-        set_motor_speed("right", -50)
-    elif angle < 0:
-        set_motor_speed("left", -50)
-        set_motor_speed("right", 50)
-    
-    time.sleep(turn_time)
-    set_motor_speed("left", 0)
-    set_motor_speed("right", 0)
+    """주어진 각도로 로봇 회전"""
+    if angle > 0:  # 우회전 (오른쪽으로 회전)
+        set_motor_direction("left", "forward")   # 왼쪽 바퀴만 전진
+        set_motor_direction("right", "stop")     # 오른쪽 바퀴는 멈춤
+    elif angle < 0:  # 좌회전 (왼쪽으로 회전)
+        set_motor_direction("left", "stop")      # 왼쪽 바퀴는 멈춤
+        set_motor_direction("right", "forward")  # 오른쪽 바퀴만 전진
+
+    time.sleep(abs(angle) / 90)  # 90도 회전하는 데 필요한 시간
+    set_motor_direction("left", "stop")
+    set_motor_direction("right", "stop")
 
 def move_forward(distance):
-    """직진"""
-    travel_time = distance / 20
-    set_motor_speed("left", 50)
-    set_motor_speed("right", 50)
-    time.sleep(travel_time)
-    set_motor_speed("left", 0)
-    set_motor_speed("right", 0)
+    """주어진 거리만큼 직진"""
+    set_motor_direction("left", "forward")
+    set_motor_direction("right", "forward")
+    time.sleep(distance / 10)  # 거리와 속도에 따른 시간 계산
+    set_motor_direction("left", "stop")
+    set_motor_direction("right", "stop")
